d8c1862fc07ead5230e537c48518e066f9f2e4c5
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-03-01 20:40:18 +0200

    Refactor app routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import styles from './App.module.scss';
 import Calendar from './components/Calendar';
-import SquadHelp from './pages/SquadHelp';
+import SquadHelp from './pages/SquadHelp/SquadHelp';
 
 class App extends Component {
-
-  render() {
+  render () {
     return (
-      <div>
-        <Calendar/>
-        {/* <SquadHelp/> */}
-      </div>
+      <Router>
+        <div>
+          <nav >
+            <ul className={styles.navLinks}>
+              <li>
+                <Link to='/calendar'>calendar</Link>
+              </li>
+              <li>
+                <Link to='/squadhelp'>form</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route exact path='/'>
+            </Route>
+            <Route exact path='/calendar' component={Calendar} />
+
+            <Route path='/squadhelp' component={SquadHelp} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
