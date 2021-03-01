9027aceee161e21f3a76fdb2b6eb558855458ec6
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-03-01 20:37:47 +0200

    Add Input

---
+++ src/components/forms/Input/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ErrorMessage, Field } from 'formik';
+import styles from './Input.module.scss';
+import cx from 'classnames';
+
+const Input = ({ name, type, ...rest }) => {
+  return (
+    <label>
+      <Field name={name}>
+        {({ field, form, meta }) => {
+          const classNames = cx(styles.input, {
+            [styles.validInput]: meta.touched && !meta.error,
+            [styles.invalidInput]: meta.touched && meta.error,
+          });
+
+          return (
+            <input type={type} {...field} className={classNames} {...rest} />
+          );
+        }}
+      </Field>
+      <ErrorMessage name={name} component='span' />
+    </label>
+  );
+};
+
+export default Input;
