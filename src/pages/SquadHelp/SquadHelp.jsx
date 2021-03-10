e944f57ede295703a69b396b7c31d6622cb8f90c
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-03-10 15:30:53 +0200

    Refactor ToS protopage

--- src/pages/SquadHelp/SquadHelp.jsx
+++ src/pages/SquadHelp/SquadHelp.jsx
@@ -62,60 +62,13 @@ const SquadHelp = props => {
               vel, tempora quae incidunt nostrum adipisci quasi! Ducimus, atque
               fuga quo illo tempora sapiente ex, veritatis excepturi nulla quasi
               mollitia eius obcaecati voluptatum quis laborum eaque nobis
-              voluptatem temporibus aspernatur vitae esse sed dolor veniam! Iure
-              itaque nulla tenetur. Quos tenetur dolores dignissimos maiores et
-              provident natus, error eum blanditiis vitae dolorum, expedita,
-              culpa sed perferendis maxime dicta. Illo alias atque omnis dolorum
-              quos laudantium, mollitia, quas, nihil corrupti expedita ad vero?
-              Ut accusantium sapiente recusandae at magni. Eius nihil ut tempora
-              quae debitis distinctio dolorum ab? Mollitia itaque dolor deserunt
-              alias reiciendis esse accusamus eum commodi! Distinctio aliquam in
-              aut sunt modi quae officiis dignissimos aspernatur quibusdam sed,
-              voluptatem, mollitia quisquam facere quia aliquid alias molestiae
-              repellendus fuga adipisci deleniti esse voluptatibus dolore! Sequi
-              ullam in iure dignissimos, laborum et corporis at ipsam aut
-              obcaecati, doloremque consequuntur nihil est dolorem, repellendus
-              rem saepe. Dignissimos nisi deleniti, quis nobis beatae deserunt
-              possimus facere unde, fugiat labore maxime corporis incidunt
-              soluta explicabo asperiores, expedita blanditiis reprehenderit
-              mollitia ipsa ab vitae ratione quos similique voluptates?
-              Distinctio error cumque nisi, sint iure molestiae debitis odio
-              neque culpa, reprehenderit consectetur molestias deserunt at eum
-              doloribus laborum voluptas expedita, provident deleniti
-              perspiciatis ullam nulla ducimus. Libero facilis saepe non earum
-              culpa ducimus, rerum soluta vel quisquam itaque natus? Id beatae
-              alias inventore quae sint ex rerum enim cupiditate doloribus unde?
-              Excepturi nam laboriosam esse, commodi modi iure. Aut repellendus
-              fuga ad doloribus natus laudantium vero consequuntur porro saepe,
-              rem praesentium beatae corporis eius reiciendis voluptas quod enim
-              dolorem. Exercitationem inventore corrupti unde harum ea esse
-              temporibus minus accusantium dolorum. Molestias tempore libero
-              quidem maxime assumenda! At ipsa dolor, tempora delectus
-              asperiores soluta maiores, eveniet cupiditate nesciunt accusamus
-              numquam modi dolorum optio earum repudiandae aspernatur aliquid
-              eum. Expedita assumenda placeat, nulla iste, iusto labore quas
-              quo, officiis cumque deleniti adipisci. Ex numquam voluptatum
-              culpa magni libero illo! Saepe laboriosam tempore adipisci eius.
-              Numquam ratione natus aut repellendus veniam a corporis, dolore
-              consequuntur sint voluptates porro ipsa tempora voluptatem
-              voluptas quod fugiat similique earum repellat ducimus saepe libero
-              dolorum vel dolores fugit! Quos alias libero, corporis minus,
-              culpa fuga possimus repellat natus eligendi rerum eius accusantium
-              esse quaerat nam quae. Impedit labore nulla perspiciatis dolore ut
-              recusandae saepe voluptatem illum consequuntur quia mollitia
-              distinctio earum, odit quibusdam ipsum consectetur commodi
-              molestias eaque expedita dolor? Perferendis, amet deleniti sint
-              illo laudantium repudiandae repellendus? Vitae temporibus suscipit
-              sunt autem molestias sapiente error dicta accusantium. Eius
-              debitis deleniti facilis tempora earum hic aperiam ratione et.
-              Atque expedita exercitationem voluptatibus, perspiciatis quaerat
-              illum beatae porro voluptatum molestias libero? Commodi, ex. Eum
-              accusamus, suscipit, vitae repellat maiores officiis alias id at
-              inventore nulla atque tempore laboriosam animi dolores culpa
-              sapiente, ad unde voluptas! Accusantium neque, minima iste
-              deserunt vero odio similique totam fugiat! Suscipit cumque
-              eligendi dolor atque earum, nam laboriosam reprehenderit nesciunt
-              minus temporibus. Earum.
+              voluptatem temporibus aspernatur vitae esse sed dolor veniam!
+            </p>
+            <h3>Lorem ipsum dolor sit amet.</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, amet
+              iusto perferendis maxime rem rerum quis ab odit! Quidem explicabo
+              mollitia ad non natus ab.
             </p>
           </div>
         </Route>
